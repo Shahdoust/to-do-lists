50f0eba505afe27fbd88adc51d389a7e29c4ebcd
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-08-23 17:17:00 +0200

    added checkbox

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-
 // Darrell
-// Declare Global Variables
-const body = document.querySelector("body");
-const itemDiv = document.querySelector(".itemDiv");
-const checkBox = document.querySelector(".checkbox");
-const listItem = document.querySelector(".listItem");
-
 // Mark to-do item
-checkBox.addEventListener("click", () => {
-    if (checkBox.checked !== false) {
-    listItem.style.textDecoration = "line-through";
-    listItem.style.color = "green";
-    const todayDate = `   Date: ${new Date().getDate()}` + `/` + `${(new Date().getMonth() + 1)}` + `/` + `${new Date().getFullYear()}, Time: ${new Date().getHours()}:` + `${new Date().getMinutes()}`;
-    dateP  = document.createElement("p");
-    dateP.setAttribute("class","date");
-    dateP.innerText = todayDate;
-    dateP.style.display = "inline";
-    dateP.style.color = "grey";
-    itemDiv.appendChild(dateP);
-    dateP.style.textDecoration = "none !important";
-    } else if (checkBox.checked !== true) {
-    itemDiv.removeChild(dateP);
-    listItem.style.textDecoration = "none";
-    listItem.style.color = "black";
-}
-    });
-console.dir(checkBox);
+// checkBox.addEventListener("click", () => {
+//     if (checkBox.checked !== false) {
+//     listItem.style.textDecoration = "line-through";
+//     listItem.style.color = "green";
+//     const todayDate = `   Date: ${new Date().getDate()}` + `/` + `${(new Date().getMonth() + 1)}` + `/` + `${new Date().getFullYear()}, Time: ${new Date().getHours()}:` + `${new Date().getMinutes()}`;
+//     dateP  = document.createElement("p");
+//     dateP.setAttribute("class","date");
+//     dateP.innerText = todayDate;
+//     dateP.style.display = "inline";
+//     dateP.style.color = "grey";
+//     itemDiv.appendChild(dateP);
+//     dateP.style.textDecoration = "none !important";
+//     } else if (checkBox.checked !== true) {
+//     itemDiv.removeChild(dateP);
+//     listItem.style.textDecoration = "none";
+//     listItem.style.color = "black";
+// }
+//     });
 // Darrell
 const sectionMain = document.querySelector(".main-section");
 
@@ -113,6 +105,27 @@ function allInOneDiv() {
     const spanTxt = document.createElement("span");
     spanTxt.classList.add("span-txt-task");
     spanTxt.textContent = textInput.value;
+    let checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    spanTxt.appendChild(checkBox);
+    checkBox.addEventListener("click", () => {
+      if (checkBox.checked !== false) {
+      spanTxt.style.textDecoration = "line-through";
+      spanTxt.style.color = "green";
+      const todayDate = `   Date: ${new Date().getDate()}` + `/` + `${(new Date().getMonth() + 1)}` + `/` + `${new Date().getFullYear()}, Time: ${new Date().getHours()}:` + `${new Date().getMinutes()}`;
+      dateP  = document.createElement("p");
+      dateP.setAttribute("class","date");
+      dateP.innerText = todayDate;
+      dateP.style.display = "inline";
+      dateP.style.color = "grey";
+      listItem.appendChild(dateP);
+      dateP.style.textDecoration = "none !important";
+      } else if (checkBox.checked !== true) {
+      listItem.removeChild(dateP);
+      spanTxt.style.textDecoration = "none";
+      spanTxt.style.color = "black";
+  }
+      });
     listItem.appendChild(spanTxt);
 
     //Create buttons remove and edit
